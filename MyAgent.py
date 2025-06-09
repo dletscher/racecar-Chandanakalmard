298a5e27f7ddc76d4b2082a25ea18570741bca19
Chandanakalmard <chandanakalmard@gmail.com>
2025-06-08 19:18:57 -0500

    Updated the logic to make the car move faster

--- MyAgent.py
+++ MyAgent.py
@@ -5,13 +5,20 @@ class Agent:
     def __init__(self):
         self.stuck_number=0
     def chooseAction(self, observations, possibleActions):
-        threshold=1.5 # This will decide the distance if the obstacle is close ie: if its close to 1.7 
-        goal_velocity=3.0# this will tell on which speed we want to reach the goal
-        low_speed=0.7
-        min_velocity=0.2# I am using this to check the min speed which i consider the car stuck
+        threshold=1.6 # This will decide the distance if the obstacle is close ie: if its close to 1.45
+        goal_velocity = 8.0
+        high_speed_goal_velocity = 11.0# this will tell on which speed we want to reach the goal
+        min_velocity=0.35# I am using this to check the min speed which i consider the car stuck
 
         lidar=observations['lidar'] # As we know we have 5 distances ie: left,front-left,front,front-right,right
         velocity=observations['velocity'] # this will capture current speed of the car 
+
+        #5 distances
+        left=lidar[0]
+        front_left=lidar[1]
+        front=lidar[2]
+        front_right=lidar[3]
+        right=lidar[4]
 
         #Here i am checking if the obstacle are close in every direction
         close_to_left_obstacle=lidar[0]<threshold
@@ -31,6 +38,8 @@ class Agent:
             self.stuck_number=0
             if ('right','accelerate') in possibleActions:
                 return ('right','accelerate')
+            elif ('left', 'accelerate') in possibleActions:
+                return ('left', 'accelerate')    
             elif ('straight','accelerate') in possibleActions:
                 return ('straight','accelerate')
             else:
@@ -41,25 +50,33 @@ class Agent:
 
         #If the obstacle is close in the front or front_left/right apply brake and turn the opposite.
         if close_to_front_obstacle or close_to_front_left_obstacle or close_to_front_right_obstacle:
-            if close_to_front_left_obstacle and ('right','brake') in possibleActions:
-                action=('right','brake')
-            elif close_to_front_right_obstacle and ('left','brake') in possibleActions:
-                action=('left','brake')
+            if right>left and ('right','brake') in possibleActions:
+                return ('right','brake')
+            elif left>=right and ('left','brake') in possibleActions:
+                return('left', 'brake')
             elif ('straight','brake') in possibleActions:
-                action=('straight','coast')
-        elif  close_to_left_obstacle and ('right','coast') in possibleActions: #if it is complete left slowly move to right and visa versa
-            action=('right','coast')
-        elif close_to_right_obstacle and ('left','coast') in possibleActions:
-            action=('left','coast')
-        else:#trying to make it speed this part is for project but tried implementing here , still implementing
-            if velocity<min_velocity:#trying to speed up the car bu checking if it is going really slow
-                if ('straight','accelerate') in possibleActions:
-                    action=('straight','accelerate')
-            elif velocity<low_speed: #if the speed is below 0.3 but not stopped or stuck  we can keep accelerating
-                action=('straight','accelerate') if ('straight','accelerate') in possibleActions else action
-            elif velocity<goal_velocity:
-                action=('straight','accelerate') if ('straight','accelerate') in possibleActions else action
-            else:
-                action=('straight','accelerate') if ('straight','accelerate') in possibleActions else action #if not slow or stuck moving
+                return ('straight','brake')
+        
+        #I am trying to void getting so close to the obstacle
+        if close_to_left_obstacle and ('right','coast') in possibleActions:
+            return ('right','coast')
+        if close_to_right_obstacle and ('left','coast') in possibleActions:
+            return ('left','coast')
+        if close_to_front_left_obstacle and ('right','coast') in possibleActions:
+            return ('right','coast')
+        if close_to_front_right_obstacle and ('left','coast') in possibleActions:
+            return ('left','coast')
+        
+        #This will speed when the front is clear 
+        if front>3.5 and velocity<high_speed_goal_velocity:
+            if ('straight','accelerate') in possibleActions:
+                return ('straight','accelerate')
+        #Speed up wen it is going slow thn the desired speed
+        if velocity<goal_velocity and ('straight','accelerate') in possibleActions:
+            return ('straight','accelerate')
 
-        return action
+        if ('straight','accelerate') in possibleActions:
+            return ('straight','accelerate')
+
+        return action                    
+        
